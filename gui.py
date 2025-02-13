0e16e50408e014e072be3c03aad09c25be946a20
RocketJakew <jan.p.schmidt@outlook.de>
2025-02-13 07:12:47 +0100

    update README

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
 # This is a GUI test in python
 
-# Goal is to create a application gui that allows the user to search the local computer for a xlsx file uploads the xlsx file and the xlsx file is then transformed in a json format which can then be sent using an api
+# Goal is to create a application gui that allows the user to search the local computer for a 
+# xlsx file uploads the xlsx file and the xlsx file is then transformed in a json format which can then be sent using an api
