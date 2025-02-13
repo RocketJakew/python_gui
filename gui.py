19ec77a84dad62e8f5fb4e96bcb245871172cb5b
Jan S. <79906048+RocketJakew@users.noreply.github.com>
2025-02-13 11:28:16 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -12,11 +12,10 @@ def on_button_click():
 def browseFiles():
  global filename
     filename = filedialog.askopenfilename(initialdir = "/",
-                                          title = "Select a File",
-                                          filetypes = (("Text files",
-                                                        "*.txt*"),
-                                                       ("all files",
-                                                        "*.*")))
+                                          title = "Select a Excel-pthFile",
+                                          filetypes = ("Excel files",
+                                                        "*.xlsx*")
+                                                       )
       
     # Change label contents
     label_file_explorer.configure(text="File Opened: "+filename)
