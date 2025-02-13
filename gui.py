913924947c499ce51e70538d901de5baf1949dd1
Jan S. <79906048+RocketJakew@users.noreply.github.com>
2025-02-13 11:06:32 +0100

    Update gui.py
    Adding functionality to browse filesystem and upload file

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-# This is a GUI test in python
+ # This is a GUI test in python
 
 # Goal is to create a application gui that allows the user to search the local computer for a 
 # xlsx file uploads the xlsx file and the xlsx file is then transformed in a json format which can then be sent using an api
@@ -9,7 +9,7 @@ def on_button_click():
 
 # Create the main window
 root = tk.Tk()
-root.title("Simple GUI")
+root.title("ESMP Converter & Upload")
 root.geometry("300x200")
 
 # Create a label
@@ -17,8 +17,11 @@ label = tk.Label(root, text="Hello, Tkinter!", font=("Arial", 14))
 label.pack(pady=20)
 
 # Create a button
-button = tk.Button(root, text="Click Me", command=on_button_click)
+button = tk.Button(root, text="Browse", command=on_button_click)
 button.pack()
+
+start_process = tk.Button(root, text="Start Process", command=start_conversion)
+start_process.pack()
 
 # Run the application
 root.mainloop()
