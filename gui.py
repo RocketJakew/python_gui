0a2d849e0fe6f7e7406788c317125efe24a45afd
Jan S. <79906048+RocketJakew@users.noreply.github.com>
2025-02-13 11:07:47 +0100

    Update gui.py
    Adding function browseFiles()

--- gui.py
+++ gui.py
@@ -6,6 +6,18 @@ import tkinter as tk
 
 def on_button_click():
     label.config(text="Button Clicked!")
+# Function to browse filesystem
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("Text files",
+                                                        "*.txt*"),
+                                                       ("all files",
+                                                        "*.*")))
+      
+    # Change label contents
+    label_file_explorer.configure(text="File Opened: "+filename)
+
 
 # Create the main window
 root = tk.Tk()
@@ -17,7 +29,7 @@ label = tk.Label(root, text="Hello, Tkinter!", font=("Arial", 14))
 label.pack(pady=20)
 
 # Create a button
-button = tk.Button(root, text="Browse", command=on_button_click)
+button = tk.Button(root, text="Browse", command=browseFiles)
 button.pack()
 
 start_process = tk.Button(root, text="Start Process", command=start_conversion)
